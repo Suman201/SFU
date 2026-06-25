0429af244a8abb60ab9ef83afcee73c180b3eaab
Suman Mandal <suman.mandal@webskitters.com>
2026-06-25 20:46:54 +0530

    update

--- apps/backend/src/notifications/notifications.module.ts
+++ apps/backend/src/notifications/notifications.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DatabaseModule } from '../database/database.module';
 import { NotificationsController } from './notifications.controller';
 import { EmailNotificationService } from './email-notification.service';
@@ -6,7 +7,7 @@ import { PushNotificationService } from './push-notification.service';
 import { NotificationsService } from './notifications.service';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuthModule, DatabaseModule],
   controllers: [NotificationsController],
   providers: [EmailNotificationService, PushNotificationService, NotificationsService],
   exports: [EmailNotificationService, PushNotificationService, NotificationsService]
